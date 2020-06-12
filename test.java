b6560d6576f5cc560be16fa25bd289398ea7c8af
Azhar2ace <66834022+Azhar2ace@users.noreply.github.com>
2020-06-12 18:11:34 +0100

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+package java_work_home;
+
+public class test {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
